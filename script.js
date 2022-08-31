9ce50487e4afa8a246940821c1b1e3c2d8e2ff90
CoryMci <corymciver@live.ca>
2022-08-31 10:51:51 -0700

    Add spaghetti code rock paper scissors with unclear way to play

---
+++ script.js
@@ -0,0 +1,80 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    return choice
+}
+
+function getPlayerChoice() {
+    let userinput = (prompt("Choose your move!")).toLowerCase();
+
+    if (userinput == "rock") {
+        console.log("Player: 0");
+        return 0;
+    }
+    else if (userinput == "paper") {
+        console.log("Player: 1");
+        return 1;
+    }
+    else if (userinput == "scissors"){
+        console.log("Player: 2");
+        return 2;
+    }
+    else {
+        console.log("Invalid move!");
+        return -1;
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    console.log("Computer: " + computerSelection)
+    let result = "No contest!";
+    if (playerSelection == computerSelection){
+        result = "draw";
+    }
+    else if (playerSelection == -1) {
+        result = "Invalid move!";
+    }
+    else if (
+        (playerSelection == 0 && computerSelection == 1) ||
+        (playerSelection == 1 && computerSelection == 2) ||
+        (playerSelection == 2 && computerSelection == 0)
+        ) {
+            result = "computer";
+        }
+    else {
+        result = "player"
+    }
+        
+    return result
+}
+
+function game() {
+    let computer = 0
+    let player = 0
+    for (let i = 0; i < 5; i++) {
+        let round = playRound(getPlayerChoice(),getComputerChoice())
+        console.log(round)
+        while (round == "draw" || round == "No contest!" || round == "Invalid move!") {
+            round = playRound(getPlayerChoice(),getComputerChoice())
+            console.log(round)
+        }
+        if (round == "computer")  {
+            computer++
+        }
+        else if (round == "player") {
+            player++
+        }
+        else {
+            console.log("error case")
+        }
+        console.log("")
+        console.log(computer)
+        console.log(player)
+        console.log("")
+
+    }
+    console.log(computer)
+    console.log(player)
+}
+
+
+game()
